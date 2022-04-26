7c6f31d797cff1107220a630925e28db80240ee1
Ericky Dos Santos <ericky@codeparticle.com>
2022-04-26 09:33:45 -0400

    DEV-191 Add env config, pages to add test rebus config to keplr wallet, and remove other config in favor of rebus

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -7,59 +7,6 @@ import { config } from 'src/config-insync';
 import { getAccount } from 'src/utils/account';
 
 const chainId = config.CHAIN_ID;
-const chainName = config.CHAIN_NAME;
-const coinDenom = config.COIN_DENOM;
-const coinMinimalDenom = config.COIN_MINIMAL_DENOM;
-const coinDecimals = config.COIN_DECIMALS;
-const prefix = config.PREFIX;
-
-const chainConfig = {
-	chainId: chainId,
-	chainName,
-	rpc: RPC_URL,
-	rest: REST_URL,
-	stakeCurrency: {
-		coinDenom,
-		coinMinimalDenom,
-		coinDecimals,
-		coinGeckoId: config.COINGECKO_ID,
-	},
-	bip44: {
-		coinType: 118,
-	},
-	bech32Config: {
-		bech32PrefixAccAddr: `${prefix}`,
-		bech32PrefixAccPub: `${prefix}pub`,
-		bech32PrefixValAddr: `${prefix}valoper`,
-		bech32PrefixValPub: `${prefix}valoperpub`,
-		bech32PrefixConsAddr: `${prefix}valcons`,
-		bech32PrefixConsPub: `${prefix}valconspub`,
-	},
-	currencies: [
-		{
-			coinDenom,
-			coinMinimalDenom,
-			coinDecimals,
-			coinGeckoId: config.COINGECKO_ID,
-		},
-	],
-	feeCurrencies: [
-		{
-			coinDenom,
-			coinMinimalDenom,
-			coinDecimals,
-			coinGeckoId: config.COINGECKO_ID,
-		},
-	],
-	coinType: config.COIN_TYPE,
-	gasPriceStep: {
-		low: config.GAS_PRICE_STEP_LOW,
-		average: config.GAS_PRICE_STEP_AVERAGE,
-		high: config.GAS_PRICE_STEP_HIGH,
-	},
-	features: config.FEATURES,
-	walletUrlForStaking: config.STAKING_URL,
-};
 
 export const initializeChain = cb => {
 	(async () => {
