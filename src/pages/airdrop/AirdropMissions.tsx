1ea7e738d87953d019216f5ca3ca8104dbf49c6c
nicolam1 <nicola@codeparticle.com>
2022-06-20 07:53:39 -0400

    DEV-277 updated airdrop content

--- src/pages/airdrop/AirdropMissions.tsx
+++ src/pages/airdrop/AirdropMissions.tsx
@@ -8,10 +8,9 @@ import { useStore } from 'src/stores';
 import useWindowSize from 'src/hooks/useWindowSize';
 
 export const ActionToDescription: { [action: string]: string } = {
-	addLiquidity: 'Add liquidity to a pool',
-	swap: 'Make a swap on Osmosis AMM',
-	vote: 'Vote on a governance proposal',
-	delegate: 'Stake OSMO',
+	stake: 'Stake $REBUS',
+	mint: 'Mint one NFTID on Rebus',
+	vault: 'Use the Rebus Vault',
 };
 
 export const AirdropMissions = observer(function AirdropMissions(props: HTMLAttributes<HTMLDivElement>) {
@@ -36,50 +35,22 @@ export const AirdropMissions = observer(function AirdropMissions(props: HTMLAttr
 				<MissionCard
 					num={0}
 					complete={!isIneligible}
-					description="Hold ATOM on February 18"
+					description="Hold and stake Atom, Osmo, or Evmos on July 15th."
 					ineligible={isIneligible}
 					isMobileView={isMobileView}
 				/>
-				{Object.entries(claimRecord.completedActions)
-					.sort(([action1], [action2]) => {
-						/*
-						 Move "vote" action to the end.
-						 Some people could think that the actions should be executed in order.
-						 But, the "vote" action can't be executed before the "stake" action.
-						 So, to reduce the confusion of new users, Move "vote" action to after the "stake" action.
-						 */
-						if (action1 === 'vote') {
-							return 1;
-						}
-						if (action2 === 'vote') {
-							return -1;
-						}
-						return 0;
-					})
-					.map(([action, value]) => {
-						return (
-							<MissionCard
-								key={action}
-								num={
-									Object.keys(claimRecord.completedActions)
-										.sort((action1, action2) => {
-											if (action1 === 'vote') {
-												return 1;
-											}
-											if (action2 === 'vote') {
-												return -1;
-											}
-											return 0;
-										})
-										.indexOf(action) + 1
-								}
-								complete={value}
-								ineligible={isIneligible}
-								description={ActionToDescription[action] ?? 'Oops'}
-								isMobileView={isMobileView}
-							/>
-						);
-					})}
+				{Object.entries(claimRecord.completedActions).map(([action, value]) => {
+					return (
+						<MissionCard
+							key={action}
+							num={Object.keys(claimRecord.completedActions).indexOf(action) + 1}
+							complete={value}
+							ineligible={isIneligible}
+							description={ActionToDescription[action] ?? 'Oops'}
+							isMobileView={isMobileView}
+						/>
+					);
+				})}
 			</MissionCardList>
 		</AirdropMissionsContainer>
 	);
