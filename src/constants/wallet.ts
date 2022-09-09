0b42a88124c1a56ab1a176f97bc59ead6bdf75c2
Ericky Dos Santos <ericky@codeparticle.com>
2022-09-08 21:26:30 -0400

    DROP-69 Disable mobile wallet and fix crash on safari

--- src/constants/wallet.ts
+++ src/constants/wallet.ts
@@ -5,6 +5,11 @@ export type WalletTypes = 'metamask' | 'crypto' | 'cosmostation' | 'falcon' | un
 	window.location.reload();
 };
 
+// Wallets do not work on safari
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+const isExtensionEnvironment = !/^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 export const WALLET_LIST: {
 	name: string;
 	description: string;
@@ -13,19 +18,20 @@ export const WALLET_LIST: {
 	walletType?: WalletTypes;
 	link?: string;
 }[] = [
-	{
+	isExtensionEnvironment && {
 		name: 'Keplr Wallet',
 		description: 'Keplr Browser Extension',
 		logoUrl: '/public/assets/other-logos/keplr.png',
 		type: 'extension',
 	},
-	{
+	// Disable mobile app for now since it is not working at all with rebus
+	false && {
 		name: 'WalletConnect',
 		description: 'Keplr Mobile',
 		logoUrl: '/public/assets/other-logos/wallet-connect.png',
 		type: 'wallet-connect',
 	},
-	{
+	isExtensionEnvironment && {
 		name: 'Metamask',
 		description: 'Metamask Browser Extension',
 		logoUrl: '/public/assets/other-logos/metamask.png',
@@ -33,7 +39,7 @@ export const WALLET_LIST: {
 		walletType: 'metamask',
 		link: 'https://metamask.io/',
 	},
-	{
+	isExtensionEnvironment && {
 		name: 'Cosmostation',
 		description: 'Cosmostation Browser Extension',
 		logoUrl: '/public/assets/other-logos/cosmostation.jpg',
@@ -41,15 +47,16 @@ export const WALLET_LIST: {
 		walletType: 'cosmostation',
 		link: 'https://www.cosmostation.io/wallet/',
 	},
-	(localStorage.getItem('falcon_enabled') === 'true' && {
-		name: 'Falcon',
-		description: 'Falcon Browser Extension',
-		logoUrl: '/public/assets/other-logos/falcon.jpg',
-		type: 'extension',
-		walletType: 'falcon',
-		link: 'https://www.falconwallet.app/',
-	}) as any,
-	{
+	(localStorage.getItem('falcon_enabled') === 'true' &&
+		isExtensionEnvironment && {
+			name: 'Falcon',
+			description: 'Falcon Browser Extension',
+			logoUrl: '/public/assets/other-logos/falcon.jpg',
+			type: 'extension',
+			walletType: 'falcon',
+			link: 'https://www.falconwallet.app/',
+		}) as any,
+	isExtensionEnvironment && {
 		name: 'Crypto.com',
 		description: 'Crypto.com Browser Extension (Transactions Not Supported)',
 		logoUrl: '/public/assets/other-logos/crypto.jpg',
