f2b4dc19a87a15010944420a073059c688191cb4
Marc Mathieu <marc@codeparticle.com>
2022-12-08 12:26:31 -0500

    add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ if (publicPath) {
 	__webpack_public_path__ = publicPath;
 }
 
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DayJsUtils from '@date-io/dayjs';
 import dayjs from 'dayjs';
@@ -52,31 +53,38 @@ dayjs.extend(duration);
 dayjs.extend(utc);
 
 const queryClient = new QueryClient();
+const theme = createMuiTheme();
+const useStyles = makeStyles(theme => {
+	root: {
+		// some CSS that accesses the theme
+	}
+});
 
 const Router: FunctionComponent = () => {
 	return (
-		<MuiPickersUtilsProvider utils={DayJsUtils}>
-			<QueryClientProvider client={queryClient}>
-				<Provider store={store}>
-					<StoreProvider>
-						<ToastProvider>
-							<AccountConnectionProvider>
-								<IBCHistoryNotifier />
-								<Terms />
-								<div className="h-screen z-0">
-									<BrowserRouter>
-										<RouteWrapper>
-											<Suspense fallback={() => <LoaderStyled />}>
-												<Switch>
-													<Route exact path="/">
-														<Redirect to={ROUTES.ASSETS} />
-													</Route>
-													<Route
-														exact
-														path={ROUTES.IBC_TRANSFER}
-														component={lazy(() => import('./pages/ibc-transfer'))}
-													/>
-													<Route
+		<ThemeProvider theme={theme}>
+			<MuiPickersUtilsProvider utils={DayJsUtils}>
+				<QueryClientProvider client={queryClient}>
+					<Provider store={store}>
+						<StoreProvider>
+							<ToastProvider>
+								<AccountConnectionProvider>
+									<IBCHistoryNotifier />
+									<Terms />
+									<div className="h-screen z-0">
+										<BrowserRouter>
+											<RouteWrapper>
+												<Suspense fallback={() => <LoaderStyled />}>
+													<Switch>
+														<Route exact path="/">
+															<Redirect to={ROUTES.ASSETS} />
+														</Route>
+														<Route
+															exact
+															path={ROUTES.IBC_TRANSFER}
+															component={lazy(() => import('./pages/ibc-transfer'))}
+														/>
+																											<Route
 														exact
 														path={[ROUTES.NFT_ID, ROUTES.NFT_ID_EDIT]}
 														component={lazy(() => import('./pages/nft-id'))}
@@ -86,36 +94,37 @@ const Router: FunctionComponent = () => {
 														path={ROUTES.NFT_ID_VIEW}
 														component={lazy(() => import('./pages/nft-id-view'))}
 													/>
-													<Route exact path={ROUTES.TOOLS} component={lazy(() => import('./pages/tools'))} />
-													<Route exact path={ROUTES.ASSETS} component={lazy(() => import('./pages/assets'))} />
-													<Route exact path={ROUTES.AIRDROP} component={lazy(() => import('./pages/airdrop'))} />
-													<Route exact path={ROUTES.STAKE} component={lazy(() => import('./pages/home'))} />
-													<Route exact path={ROUTES.VOTE} component={lazy(() => import('./pages/proposals'))} />
-													<Route
-														exact
-														path={`${ROUTES.VOTE}/:proposalId`}
-														component={lazy(() => import('./pages/proposal-details'))}
-													/>
-													<Route
-														exact
-														path={ROUTES.WALLET_CONNECT}
-														component={lazy(() => import('./pages/wallet-connect'))}
-													/>
-													<Route>
-														<NotFoundPage />
-													</Route>
-												</Switch>
-											</Suspense>
-										</RouteWrapper>
-									</BrowserRouter>
-								</div>
-								<ToastContainer transition={Bounce} />
-							</AccountConnectionProvider>
-						</ToastProvider>
-					</StoreProvider>
-				</Provider>
-			</QueryClientProvider>
-		</MuiPickersUtilsProvider>
+														<Route exact path={ROUTES.TOOLS} component={lazy(() => import('./pages/tools'))} />
+														<Route exact path={ROUTES.ASSETS} component={lazy(() => import('./pages/assets'))} />
+														<Route exact path={ROUTES.AIRDROP} component={lazy(() => import('./pages/airdrop'))} />
+														<Route exact path={ROUTES.STAKE} component={lazy(() => import('./pages/home'))} />
+														<Route exact path={ROUTES.VOTE} component={lazy(() => import('./pages/proposals'))} />
+														<Route
+															exact
+															path={`${ROUTES.VOTE}/:proposalId`}
+															component={lazy(() => import('./pages/proposal-details'))}
+														/>
+														<Route
+															exact
+															path={ROUTES.WALLET_CONNECT}
+															component={lazy(() => import('./pages/wallet-connect'))}
+														/>
+														<Route>
+															<NotFoundPage />
+														</Route>
+													</Switch>
+												</Suspense>
+											</RouteWrapper>
+										</BrowserRouter>
+									</div>
+									<ToastContainer transition={Bounce} />
+								</AccountConnectionProvider>
+							</ToastProvider>
+						</StoreProvider>
+					</Provider>
+				</QueryClientProvider>
+			</MuiPickersUtilsProvider>
+		</ThemeProvider>
 	);
 };
 
