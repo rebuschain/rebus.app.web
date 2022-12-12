5e9f8f4c0e32e78c1e092841efbae7c1b5805f79
Marc Mathieu <marc@codeparticle.com>
2022-12-12 12:59:34 -0500

    fix confirm dialog

--- src/pages/stake/delegate-dialog/confirm-dialog.tsx
+++ src/pages/stake/delegate-dialog/confirm-dialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent } from '@mui/material';
 import { ResultDialogHeader, ResultDialogText } from './components';
 
 type Props = {
