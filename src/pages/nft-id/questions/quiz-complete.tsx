8ecd973fdce8fcf304a412fc81accc091f889e37
Marc Mathieu <marc@codeparticle.com>
2022-12-13 15:37:26 -0500

    rebase and refactor to make new changes work with react 18

--- src/pages/nft-id/questions/quiz-complete.tsx
+++ src/pages/nft-id/questions/quiz-complete.tsx
@@ -13,42 +13,44 @@ type QuizCompleteProps = {
 	onClick: () => void;
 };
 
-const QuizComplete: FunctionComponent<React.PropsWithChildren<QuizCompleteProps>> = observer(({ creatingIdRecord, hasPassed, onClick }) => {
-	const { questionsStore } = useStore();
+const QuizComplete: FunctionComponent<React.PropsWithChildren<QuizCompleteProps>> = observer(
+	({ creatingIdRecord, hasPassed, onClick }) => {
+		const { questionsStore } = useStore();
 
-	const onDocumentationClick = useCallback(() => {
-		window.open('https://medium.com/@RebusChain/nftid-nifdy-a-new-take-on-identity-with-rebus-76cb074a1dba');
-	}, []);
+		const onDocumentationClick = useCallback(() => {
+			window.open('https://medium.com/@RebusChain/nftid-nifdy-a-new-take-on-identity-with-rebus-76cb074a1dba');
+		}, []);
 
-	return (
-		<>
-			{questionsStore.isTotalScoreFetching ? (
-				<Loader />
-			) : (
-				<>
-					<div className="image-wrapper">
-						<img alt="quiz-complete" src={hasPassed ? quizPassed : quizFailed} />
-					</div>
-					<p className="title text-center text-xl-2 py-6 leading-8">
-						{hasPassed ? 'Congrats!' : 'Unfortunately, you didn’t pass.'}
-					</p>
-					<p className="sub-text text-center text-base text-white-mid px-8 mb-3">
-						{hasPassed
-							? 'You passed! You may now create your NFTID.'
-							: 'Hit our docs and study up, you can try again as soon as you think you’re ready.'}
-					</p>
+		return (
+			<>
+				{questionsStore.isTotalScoreFetching ? (
+					<Loader />
+				) : (
+					<>
+						<div className="image-wrapper">
+							<img alt="quiz-complete" src={hasPassed ? quizPassed : quizFailed} />
+						</div>
+						<p className="title text-center text-xl-2 py-6 leading-8">
+							{hasPassed ? 'Congrats!' : 'Unfortunately, you didn’t pass.'}
+						</p>
+						<p className="sub-text text-center text-base text-white-mid px-8 mb-3">
+							{hasPassed
+								? 'You passed! You may now create your NFTID.'
+								: 'Hit our docs and study up, you can try again as soon as you think you’re ready.'}
+						</p>
 
-					<Button
-						backgroundStyle="blue"
-						disabled={creatingIdRecord}
-						onClick={hasPassed ? onClick : onDocumentationClick}
-						smallBorderRadius>
-						{hasPassed ? 'Create NFT ID' : 'GO TO DOCUMENTATION'}
-					</Button>
-				</>
-			)}
-		</>
-	);
-});
+						<Button
+							backgroundStyle="blue"
+							disabled={creatingIdRecord}
+							onClick={hasPassed ? onClick : onDocumentationClick}
+							smallBorderRadius>
+							{hasPassed ? 'Create NFT ID' : 'GO TO DOCUMENTATION'}
+						</Button>
+					</>
+				)}
+			</>
+		);
+	}
+);
 
 export default QuizComplete;
