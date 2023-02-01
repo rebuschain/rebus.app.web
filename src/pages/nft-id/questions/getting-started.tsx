a29a2e182ee0596987be5e727def2456451fad2a
Ericky Dos Santos <ericky@codeparticle.com>
2023-02-01 15:17:54 -0500

    DEV-453 Show error messaging if attempting to create nftid while not having an account

--- src/pages/nft-id/questions/getting-started.tsx
+++ src/pages/nft-id/questions/getting-started.tsx
@@ -1,11 +1,22 @@
+import styled from '@emotion/styled';
 import React, { FunctionComponent } from 'react';
 import gettingStarted from 'src/assets/nft-id-quiz/quiz-passing.png';
+import { Loader } from 'src/components/common/loader';
 
 type GettingStartedProps = {
+	disabled?: boolean;
 	onClick: () => void;
 };
 
-const GettingStarted: FunctionComponent<GettingStartedProps> = ({ onClick }) => {
+const loaderStyles: React.CSSProperties = {
+	height: '100%',
+	justifyContent: 'left',
+	left: '18px',
+	position: 'absolute',
+	top: '0',
+};
+
+const GettingStarted: FunctionComponent<GettingStartedProps> = ({ disabled, onClick }) => {
 	return (
 		<>
 			<div className="image-wrapper">
@@ -17,8 +28,8 @@ const GettingStarted: FunctionComponent<GettingStartedProps> = ({ onClick }) => 
 			<p className="sub-text text-center text-base text-white-mid px-8">
 				You will be asked 5 questions at random, you need to answer at least 4 of them correctly.
 			</p>
-			<button onClick={onClick} className="bg-blue1 py-2 px-15 rounded-lg mt-6">
-				Get Started
+			<button disabled={disabled} onClick={onClick} className="bg-blue1 py-2 px-15 rounded-lg mt-6 relative">
+				{disabled && <Loader className="h-7" style={loaderStyles} />}Get Started
 			</button>
 		</>
 	);
