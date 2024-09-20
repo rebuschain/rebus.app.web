55cdc8c51458101b0440b5bb557fe4b7cd092354
vertotrade <nicola@codeparticle.com>
2024-09-20 12:15:32 -0400

    Added function for static APIs

---
+++ api/feature-flags.js
@@ -0,0 +1,10 @@
+import { loadJSONFile } from './utils/load-json-file';
+
+export default async function handler(req, res) {
+  try {
+    const data = await loadJSONFile('feature-flags');  // Load specific JSON file
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
